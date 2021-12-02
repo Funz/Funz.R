331dfaf80756d34962d73869c03d86f22c085503
Yann Richet <richetyann@gmail.com>
2021-12-02 16:28:09 +0100

    avoid inf loop

--- inst/Funz/scripts/R.bat
+++ inst/Funz/scripts/R.bat
@@ -4,15 +4,17 @@ set PATH=%PATH%;C:\Program Files\R\R-4.1.0\bin
 
 set id=%RANDOM%
 start "R_%id%" cmd /c "R.exe CMD BATCH %*"
+set PID_R="none"
 for /F "TOKENS=1,2,*" %%a in ('tasklist /FI "WINDOWTITLE eq R_%id%"') do set PID_R=%%b
 echo %PID_R% > PID
 
-
+if "%PID_R%" != "none" (
 :loop
 tasklist | findstr " %PID_R% " >nul
 if not errorlevel 1 (
     timeout /t 1 >nul
     goto :loop
+)
 )
 
 del /f PID
